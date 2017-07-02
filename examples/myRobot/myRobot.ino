243f0993b245ad7036074bd366f89be21758954d
Saleem Hadad <saleem_ha@hotmail.com>
2017-07-02 15:34:13 +0800

    update example

--- examples/myRobot/myRobot.ino
+++ examples/myRobot/myRobot.ino
@@ -4,27 +4,14 @@
 #include <BBot.h>
 #include <string.h>
 
-int leftIR = 37, middleIR = 38, rightIR = 39;
-int motorA1 = 9, motorA2 = 8, motorB1 = 10, motorB2 = 11;
-int redLED = 2, greenLED = 3, buzzer= 4;
-int a = 46, b = 47, c = 43, d = 45, e = 44, f = 49, g = 48;
-int trig = 6, echo = 7;
+int motorA1 = 2, motorA2 = 3, motorB1 = 4, motorB2 = 5;
+
 String str;
 #define SS_PIN 53
 #define RST_PIN 5
 
 MFRC522 rfid(SS_PIN, RST_PIN);
-MFRC522::MIFARE_Key key;
-SoftwareSerial serial(2,3);
-BBot myRobot(serial);
-
-void RFID(){
-  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) { return; }
-  int cardId = rfid.uid.uidByte[0] + rfid.uid.uidByte[1] + rfid.uid.uidByte[2] + rfid.uid.uidByte[3];
-  myRobot.RFID(cardId);
-  rfid.PICC_HaltA();
-  rfid.PCD_StopCrypto1();
-}
+BBot myRobot(rfid);
 
 void serialEvent(){
   while(Serial.available()) {
@@ -34,20 +21,11 @@ void serialEvent(){
 }
 
 void setup() {
-  pinMode(leftIR,INPUT); pinMode(middleIR,INPUT); pinMode(rightIR,INPUT);
   pinMode(motorA1,OUTPUT); pinMode(motorA2,OUTPUT); pinMode(motorB1,OUTPUT); pinMode(motorB2,OUTPUT);
-  pinMode(redLED, OUTPUT); pinMode(greenLED, OUTPUT); pinMode(buzzer, OUTPUT); dw(redLED, 0); dw(greenLED, 0); dw(buzzer, 0);
-  pinMode(a, OUTPUT); pinMode(b, OUTPUT); pinMode(c, OUTPUT); pinMode(d, OUTPUT); pinMode(e, OUTPUT); pinMode(f, OUTPUT); pinMode(g, OUTPUT);
-  pinMode(echo, INPUT); pinMode(trig, OUTPUT);
   myRobot.setupMotors(motorA1, motorA2, motorB1, motorB2);
-  myRobot.setupIRs(leftIR, middleIR, rightIR);
-  myRobot.setupSevenSegment(a,b,c,d,e,f,g);
-  myRobot.setupUltrasonic(trig, echo);
-  myRobot.setupCircuitComponents(redLED, greenLED, buzzer);
-  myRobot.setMode(LineFollowing);
+  myRobot.setMode(Teleoperation);
   Serial.begin(115200); Serial.flush(); Serial.setTimeout(0);
   SPI.begin(); rfid.PCD_Init();
-  for (byte i = 0; i < 6; i++) { key.keyByte[i] = 0xFF; }
 }
 
 void loop(){
